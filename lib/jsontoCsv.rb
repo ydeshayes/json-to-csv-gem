d123ca950088a58a09a1867e88de476e2dfbb9f8
ydeshayes <deshayes.yann@gmail.com>
2017-11-19 10:49:27 +0100

    Add the extractKeys function

--- lib/jsontoCsv.rb
+++ lib/jsontoCsv.rb
@@ -1,5 +1,38 @@
 class JsonToCsv
+  # Convert a json array string to csv
+  # return string
   def self.convert(json = "[]")
     puts "Hello word2"
   end
+
+  # Extract keys from a hash, will concat nested keys
+  # with a .
+  # return Array[string]
+  def self.extractKeys(hash = {}, parentsName = "")
+    hash.each_with_object([]) do |(k,v),keys|
+      if v.is_a? Hash
+        keys.concat(self.extractKeys(v, k + "."))
+      else
+        keys << parentsName + k
+      end
+    end
+  end
+<<<<<<< Updated upstream
+=======
+
+  # Extract values from a hash, will concat array values
+  # with a ,
+  # return Array[string]
+  def self.extractValues(hash = {})
+    hash.each_with_object([]) do |(k,v),values|
+      if v.is_a? Hash
+        values.concat(self.extractValues(v))
+      elsif v.kind_of? Array
+        values << v.join(',')
+      else
+        values << v
+      end
+    end
+  end
+>>>>>>> Stashed changes
 end
